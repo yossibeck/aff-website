7d71690abe46d8f220163276c9a017de05665720
Yossi Beck <yossibeck@gmail.com>
2026-04-19 15:36:37 +0300

    feat: add Astro.locals type augmentation for tenant + D1

---
+++ src/env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="astro/client" />
+
+interface Env {
+  DB: D1Database;
+}
+
+type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
+
+declare namespace App {
+  interface Locals extends Runtime {
+    tenant: import('./lib/db').Tenant;
+  }
+}
