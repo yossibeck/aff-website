cb85567c3a5509e109cb4f27d120218a0aaa79d5
Yossi Beck <yossibeck@gmail.com>
2026-04-19 15:33:13 +0300

    feat: configure Cloudflare adapter, D1 binding, TS types

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  output: 'server',
+  adapter: cloudflare(),
 });
